a0f3cf7dfdccfc666265eaef43bada04e4e0be65
Natalie Clark <nj_clark@hotmail.co.uk>
2023-10-04 11:11:12 +0100

    Add app for books

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
 from lib.database_connection import DatabaseConnection
-from lib.artist_repository import ArtistRepository
-
+from lib.book_repository import BookRepository
 
 # Connect to the database
 connection = DatabaseConnection()
 connection.connect()
 
 # Seed with some seed data
-connection.seed("seeds/music_library.sql")
+connection.seed("seeds/book_store.sql")
 
-# Retrieve all artists
-artist_repository = ArtistRepository(connection)
-artists = artist_repository.all()
-
-# List them out
-for artist in artists:
-    print(artist)
+# Retieve all books
+book_repository = BookRepository(connection)
+for book in book_repository.all():
+    print(book)
