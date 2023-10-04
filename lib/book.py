a221ab4364f323abdc905ba86b9fd81e0f452b7e
Natalie Clark <nj_clark@hotmail.co.uk>
2023-10-04 11:10:22 +0100

    Add __eq__ and __repr__ methods to Book class

--- lib/book.py
+++ lib/book.py
@@ -3,3 +3,9 @@ class Book:
         self.id = id
         self.title = title
         self.author_name = author_name
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+    
+    def __repr__(self):
+        return f"{self.id} - {self.title} - {self.author_name}"
